b92a7208770bdd5769df75d4ab1396587ba41026
Fauroro <151984072+Fauroro@users.noreply.github.com>
2024-08-13 17:06:06 +0200

    feat: :construction: add impor.sql
    add impor.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO country (codecountry,namecountry) VALUES ("COL","Colombia")
+INSERT INTO region (codecountry,codereg,namereg) VALUES ("COL","CUND","Cundinamarca")
+INSERT INTO city (codecity,codereg,namecity) VALUES ("BOG","CUND","Bogota")
+INSERT INTO farmacy(namefarmacy,addressfarmacy,longfarmacy,codecityfarm,latfarmacy,logofarmacy) VALUES ("farmacia A","Calle 123",55.2,"BOG",47.5,"logo.jpg");
